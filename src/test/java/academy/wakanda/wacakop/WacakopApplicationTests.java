dcdedf57a1124e2a67a6778d15ea3754c9cbc266
kinhoLisboa <danifleikson@hotmail.com>
2024-01-22 20:11:13 -0300

    Iniciando os trabalhos

---
+++ src/test/java/academy/wakanda/wacakop/WacakopApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.wakanda.wacakop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WacakopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
