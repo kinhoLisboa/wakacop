8c0e95085863da4f1bf56dbd43d37f80b5c93962
kinhoLisboa <danifleikson@hotmail.com>
2024-01-23 18:44:36 -0300

    Implementa dominio de pauta

---
+++ src/main/java/academy/wakanda/wacakop/pauta/domain/Pauta.java
@@ -0,0 +1,22 @@
+package academy.wakanda.wacakop.pauta.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@ToString
+@Getter
+public class Pauta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(columnDefinition = "uuid", updatable = false, unique = true, nullable = false)
+    private UUID id;
+    private String titulo;
+    private String descrica;
+    private UUID idAssociado;
+    private LocalDateTime dataCriacao
+}
